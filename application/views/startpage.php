166568cdf01689fa8b5cc1c1c0ab81956199f52c
Alex Fedorets <santik.alex@gmail.com>
2012-11-06 20:57:27 +0200

    homepage fixes

--- application/views/startpage.php
+++ application/views/startpage.php
@@ -43,9 +43,76 @@
             <div class="sidetitle">
                 <h1>Search</h1>
             </div>
+            <div class="part">
+                <input name="Search the site" type="text" maxlength="22" />
+                <input type="submit" name="submit" value="Search" />
+                </div>
             <div class="sidetitle">
                 <h1>Top Tags</h1>
             </div>
+            <div class="part">
+                <a href="http://v1.perfect-look.org/tags/fashion" class="style1">fashion</a>,<a href="http://v1.perfect-look.org/tags/hair"> hair</a>, <a href="http://v1.perfect-look.org/tags/makeup" class="style2">makeup</a>, <a href="http://v1.perfect-look.org/tags/nails">nails</a>, <a href="http://v1.perfect-look.org/tags/celebs">celebs</a>, <a href="http://v1.perfect-look.org/tags/personal" class="style3">personal</a>
+            </div>
+            <div class="sidetitle">
+                <h1>Latest Announcements</h1>
+            </div>
+            <div class="part">
+                <ul>
+                    <li><a href="http://news.perfect-look.org/index.php?subaction=showcomments&amp;id=1351398638&amp;archive=&amp;start_from=&amp;ucat=&amp;">Perfect-Look.Org Free Hosting</a></li>
+                    <li><a href="http://news.perfect-look.org/index.php?subaction=showcomments&amp;id=1351220972&amp;archive=&amp;start_from=&amp;ucat=&amp;">Looking for feedback sessions helpers</a></li>
+                    <li><a href="http://news.perfect-look.org/index.php?subaction=showcomments&amp;id=1351180511&amp;archive=&amp;start_from=&amp;ucat=&amp;">Secret Items for the first 1000 users released</a></li>
+                    <li><a href="http://news.perfect-look.org/index.php?subaction=showcomments&amp;id=1351025166&amp;archive=&amp;start_from=&amp;ucat=&amp;">New Halloween Items</a></li>
+                    <li><a href="http://news.perfect-look.org/index.php?subaction=showcomments&amp;id=1349977231&amp;archive=&amp;start_from=&amp;ucat=&amp;">Perfect-Look.Org Promo Contest</a></li>
+                </ul>
+            </div>
+
+            <div class="sidetitle">
+                <h1>fACEBOOK BOX</h1>
+            </div>
+
+            <div class="sidetitle">
+                <h1>Featured Users</h1>
+            </div>
+            <div class="part">
+                <a href="http://v1.perfect-look.org/diirectioner"><img src="images/user01.jpg" width="60" height="60" /></a> <a href="http://v1.perfect-look.org/taylorswift"><img src="images/user02.jpg" width="60" height="60" /></a> <a href="http://v1.perfect-look.org/_ibiebsswag"><img src="images/user03.jpg" width="60" height="60" /></a> <a href="http://v1.perfect-look.org/bdaysuits"><img src="images/user04.jpg" width="60" height="60" /></a></a> <a href="http://v1.perfect-look.org/perfectlookadmin"><img src="images/user05.jpg" width="60" height="60" /></a>
+            </div>
+
+            <div class="sidetitle">
+                <h1>LATEST DRESSUP ITEMS</h1>
+            </div>
+            <div class="part">
+                <table width="100%" border="0" cellspacing="3" cellpadding="0">
+                    <tr>
+                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-bat.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-cauldron.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-witchhat.png" width="60" height="60" /></div></td>
+                    </tr>
+                    <tr>
+                        <td><div align="center"><a href="http://v1.perfect-look.org/item/bat">Bat</a></div></td>
+                        <td><div>
+                            <div align="center"><a href="http://v1.perfect-look.org/item/cauldron">Cauldron </a></div>
+                        </div></td>
+                        <td><div>
+                            <div align="center"><a href="http://v1.perfect-look.org/item/witchhat">Witch Hat </a></div>
+                        </div></td>
+                    </tr>
+                    <tr>
+                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-witchoutfit.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-shoes-halloween.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/innerwear/profilepics/Profilepics-Innerwear-stockings1.png" width="60" height="60" /></div></td>
+                    </tr>
+                    <tr>
+                        <td><div align="center">
+                            <div> <a href="http://v1.perfect-look.org/item/witchoutfit">Witch Outfit</a> </div>
+                        </div></td>
+                        <td><div align="center">
+                            <div><a href="http://v1.perfect-look.org/item/shoes-halloween"> Halloween Shoes </a></div>
+                        </div></td>
+                        <td><div align="center"><a href="http://v1.perfect-look.org/item/stockings">Stockings</a></div></td>
+                    </tr>
+                </table>
+            </div>
+
 
         </div>
     </div>
