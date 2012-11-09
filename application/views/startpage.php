c0d51e7c9f2f71798030fd44138f96d1c78a9c82
Alex Fedorets <santik.alex@gmail.com>
2012-11-09 08:03:25 +0200

    homepage infinite scroll

--- application/views/startpage.php
+++ application/views/startpage.php
@@ -50,7 +50,7 @@
                 <h1>Top Tags</h1>
             </div>
             <div class="part">
-                <a href="http://v1.perfect-look.org/tags/fashion" class="style1">fashion</a>,<a href="http://v1.perfect-look.org/tags/hair"> hair</a>, <a href="http://v1.perfect-look.org/tags/makeup" class="style2">makeup</a>, <a href="http://v1.perfect-look.org/tags/nails">nails</a>, <a href="http://v1.perfect-look.org/tags/celebs">celebs</a>, <a href="http://v1.perfect-look.org/tags/personal" class="style3">personal</a>
+                <a href="/tags/fashion" class="style1">fashion</a>,<a href="/tags/hair"> hair</a>, <a href="/tags/makeup" class="style2">makeup</a>, <a href="/tags/nails">nails</a>, <a href="/tags/celebs">celebs</a>, <a href="/tags/personal" class="style3">personal</a>
             </div>
             <div class="sidetitle">
                 <h1>Latest Announcements</h1>
@@ -69,7 +69,7 @@
                 <h1>Featured Users</h1>
             </div>
             <div class="part">
-                <a href="http://v1.perfect-look.org/diirectioner"><img src="images/user01.jpg" width="60" height="60" /></a> <a href="http://v1.perfect-look.org/taylorswift"><img src="images/user02.jpg" width="60" height="60" /></a> <a href="http://v1.perfect-look.org/_ibiebsswag"><img src="images/user03.jpg" width="60" height="60" /></a> <a href="http://v1.perfect-look.org/bdaysuits"><img src="images/user04.jpg" width="60" height="60" /></a></a> <a href="http://v1.perfect-look.org/perfectlookadmin"><img src="images/user05.jpg" width="60" height="60" /></a>
+                <a href="/diirectioner"><img src="images/user01.jpg" width="60" height="60" /></a> <a href="/taylorswift"><img src="images/user02.jpg" width="60" height="60" /></a> <a href="/_ibiebsswag"><img src="images/user03.jpg" width="60" height="60" /></a> <a href="/bdaysuits"><img src="images/user04.jpg" width="60" height="60" /></a></a> <a href="/perfectlookadmin"><img src="images/user05.jpg" width="60" height="60" /></a>
             </div>
 
             <div class="sidetitle">
@@ -78,32 +78,32 @@
             <div class="part">
                 <table width="100%" border="0" cellspacing="3" cellpadding="0">
                     <tr>
-                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-bat.png" width="60" height="60" /></div></td>
-                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-cauldron.png" width="60" height="60" /></div></td>
-                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-witchhat.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="/files/items/halloween/profilepics/profilepics-bat.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="/files/items/halloween/profilepics/profilepics-cauldron.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="/files/items/halloween/profilepics/profilepics-witchhat.png" width="60" height="60" /></div></td>
                     </tr>
                     <tr>
-                        <td><div align="center"><a href="http://v1.perfect-look.org/item/bat">Bat</a></div></td>
+                        <td><div align="center"><a href="/item/bat">Bat</a></div></td>
                         <td><div>
-                            <div align="center"><a href="http://v1.perfect-look.org/item/cauldron">Cauldron </a></div>
+                            <div align="center"><a href="/item/cauldron">Cauldron </a></div>
                         </div></td>
                         <td><div>
-                            <div align="center"><a href="http://v1.perfect-look.org/item/witchhat">Witch Hat </a></div>
+                            <div align="center"><a href="/item/witchhat">Witch Hat </a></div>
                         </div></td>
                     </tr>
                     <tr>
-                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-witchoutfit.png" width="60" height="60" /></div></td>
-                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/halloween/profilepics/profilepics-shoes-halloween.png" width="60" height="60" /></div></td>
-                        <td><div align="center"><img src="http://v1.perfect-look.org/files/items/innerwear/profilepics/Profilepics-Innerwear-stockings1.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="/files/items/halloween/profilepics/profilepics-witchoutfit.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="/files/items/halloween/profilepics/profilepics-shoes-halloween.png" width="60" height="60" /></div></td>
+                        <td><div align="center"><img src="/files/items/innerwear/profilepics/Profilepics-Innerwear-stockings1.png" width="60" height="60" /></div></td>
                     </tr>
                     <tr>
                         <td><div align="center">
-                            <div> <a href="http://v1.perfect-look.org/item/witchoutfit">Witch Outfit</a> </div>
+                            <div> <a href="/item/witchoutfit">Witch Outfit</a> </div>
                         </div></td>
                         <td><div align="center">
-                            <div><a href="http://v1.perfect-look.org/item/shoes-halloween"> Halloween Shoes </a></div>
+                            <div><a href="/item/shoes-halloween"> Halloween Shoes </a></div>
                         </div></td>
-                        <td><div align="center"><a href="http://v1.perfect-look.org/item/stockings">Stockings</a></div></td>
+                        <td><div align="center"><a href="/item/stockings">Stockings</a></div></td>
                     </tr>
                 </table>
             </div>
