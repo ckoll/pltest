d149a147097423303aa42569c5ea72563018eba0
Alex Fedorets <santik.alex@gmail.com>
2012-11-16 16:56:30 +0200

    photos grid

--- application/views/main/explore_latest_photos.php
+++ application/views/main/explore_latest_photos.php
@@ -10,29 +10,34 @@
     </div>
     <div class="bg">
         <div style="margin-left: 50px">
-        <?
-        if (!empty($photos)) {
-            foreach ($photos as $val) {
-                ?>
-                <div class="photo_tumb">
-                    <?php  if (!empty($admin)): ?>
-                    <a style="height: auto; width: auto" href="/user/delete_photo/?id=<?= $val['id'] . $val['rand_num'] ?>"><img src="/images/del.png"></a>
-                    <?php endif; ?>
-                    <a href="/<?= $this->user['username'] ?>/photo/<?= $val['id'] . $val['rand_num'] ?>" style="background-image: url('/files/users/uploads/<?= $val['uid'] ?>/<?= $val['id'] . $val['rand_num'] ?>.<?=$val['image_type']?>')"></a><br>
-                    <span class="likes" data-id="<?=$val['id'] . $val['rand_num']?>" data-mode="upload"><?= $val['like'] ?></span>
-                    <span class="comments"><?= $val['comments'] ?></span>
-                    <!--<img src="/images/del.png" data-id="<?= $val['id'] . $val['rand_num'] ?>" class="right del_photo">-->
+            <?
+            if (!empty($photos)) {
+                foreach ($photos as $val) {
+                    ?>
+                    <div class="photo_tumb">
+                        <?php  if (!empty($admin)): ?>
+                        <a style="height: auto; width: auto"
+                           href="/user/delete_photo/?id=<?= $val['id'] . $val['rand_num'] ?>"><img
+                                src="/images/del.png"></a>
+                        <?php endif; ?>
+                        <a href="/<?= $this->user['username'] ?>/photo/<?= $val['id'] . $val['rand_num'] ?>">
+                        <img src="/files/users/uploads/<?= $val['uid'] ?>/<?= $val['id'] . $val['rand_num'] ?>.<?=$val['image_type']?>" alt="" width="130">
+                        </a><br>
+                        <span class="likes" data-id="<?=$val['id'] . $val['rand_num']?>"
+                              data-mode="upload"><?= $val['like'] ?></span>
+                        <span class="comments"><?= $val['comments'] ?></span>
+                        <!--<img src="/images/del.png" data-id="<?= $val['id'] . $val['rand_num'] ?>" class="right del_photo">-->
 
-                </div>
-                <?
+                    </div>
+                    <?
+                }
+            } else {
+                ?><em>*No photos</em><?
             }
-        } else {
-            ?><em>*No photos</em><?
-    }
-        ?>
+            ?>
         </div>
-    <br class="clear">
-    <?pagination($pages)?>
+        <br class="clear">
+        <?pagination($pages)?>
     </div>
     <div class="footer"></div>
 </div>
