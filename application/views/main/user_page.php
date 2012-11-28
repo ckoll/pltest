3ae878087fe32a204c366b0387c28c577d9866b9
Alex Fedorets <santik.alex@gmail.com>
2012-11-28 10:59:15 +0200

    latest photos user

--- application/views/main/user_page.php
+++ application/views/main/user_page.php
@@ -78,8 +78,9 @@
                         </div>
                         <p>
                             <?= time_from($val['date']) ?>
-                            <span class="hearts <?=!$val['liked']?'grey':''?> likes" data-id="<?= $val['id'] . $val['rand_num'] ?>" data-mode="upload"  data-type="<?=$val['liked']?'remove':'add'?>"><?=$val['like']?></span>
-                            <span class="comments"><?=intval($val['comments'])?></span>
+                            <br>
+                            <span style="display: inline-block; margin: 3px auto;" class="hearts <?=!$val['liked']?'grey':''?> likes" data-id="<?= $val['id'] . $val['rand_num'] ?>" data-mode="upload"  data-type="<?=$val['liked']?'remove':'add'?>"><?=$val['like']?></span>
+                            <span style="display: inline-block" class="comments"><?=intval($val['comments'])?></span>
                         </p>
                     </li>
                     <?
