fbb892f5a62c831c200262ccf76f6d4c0e5ceb09
Alex Fedorets <santik.alex@gmail.com>
2012-11-28 12:22:19 +0200

    latest photos buttons

--- application/views/js/mystuff_init.js
+++ application/views/js/mystuff_init.js
@@ -1,12 +1,14 @@
 $(function(){
     $('.delete_photo').click(function(){
-        var id = $(this).data('id')
-        $.post('/upload/ajax',{
-            'func': 'del_uploaded_img',
-            'id': id
-        },function(){
-            document.location = document.URL;
-        },'json');
+        if (confirm('Are you sure you want to delete this photo?')) {
+            var id = $(this).data('id')
+            $.post('/upload/ajax',{
+                'func': 'del_uploaded_img',
+                'id': id
+            },function(){
+                document.location = document.URL;
+            },'json');
+        }
         return false;
     });
     $('.my_photo').click(function(){
--- application/views/mystuff/photos.php
+++ application/views/mystuff/photos.php
@@ -10,22 +10,24 @@
         <?
         if (!empty($photos))
             foreach ($photos as $photo) {
-            ?><div data-url="<?=  base_url()?><?=$user['username']?>/photo/<?=$photo['photo_id']?>" class="my_photo">
-                <?
-                if (method_exists('Mystuff', 'photos') && $this->user['id'] == $user['id']) {
-                    ?>
-                <div class="photo_edit">
-                        <a href="/upload/photo_upload/<?=$photo['photo_id']?>/edit"><img src="/images/edit.png"></a>
-                        <img class="delete_photo" data-id="<?=$photo['photo_id']?>" src="/images/del.gif">
-                        
-                    </div>
-                        
-                        
-                    <? } ?>
-                    <img style="max-width: 130px; max-height: 130px;" src='/files/users/uploads/<?= $user['id'] . '/' . $photo['photo_id'] . '.'.$photo['image_type'] ?>'>
+                ?>
+                <div data-url="<?=  base_url()?><?=$user['username']?>/photo/<?=$photo['photo_id']?>" class="my_photo">
+                    <?
+                    if (method_exists('Mystuff', 'photos') && $this->user['id'] == $user['id']) {
+                        ?>
+                        <div class="photo_edit">
+                            <a href="/upload/photo_upload/<?=$photo['photo_id']?>/edit"><img src="/images/edit.png"></a>
+                            <img class="delete_photo" data-id="<?=$photo['photo_id']?>" src="/images/del.gif">
+
+                        </div>
+
+
+                        <? } ?>
+                    <img style="max-width: 130px; max-height: 130px;"
+                         src='/files/users/uploads/<?= $user['id'] . '/' . $photo['photo_id'] . '.' . $photo['image_type'] ?>'>
                 </div><?
-        }
-            ?>
+            }
+        ?>
         <br class="clear">
         <? pagination($pages); ?>
     </div>
