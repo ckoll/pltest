dca514a7e46c69983e126f969be6477d9b6939f9
Alex Fedorets <santik.alex@gmail.com>
2012-11-29 17:38:52 +0200

    eyes

--- application/models/dressup_model.php
+++ application/models/dressup_model.php
@@ -47,10 +47,10 @@ class Dressup_model extends CI_Model {
                 if (!empty($items_body)) {
                     foreach ($items_body as $val) {
                         if (in_array($val['name'], array('normal skin eyes', 'olive skin eyes', 'dark skin eyes'))) {
-                            continue;
+                            //continue;
                         }
                         $type = str_replace('files/', '', $val['directory']);
-                        $items[] = array('id' => $val['name'], 'item_name' => $val['name'], 'profileimage_dir' => 'profilepics', 'profileimage' => $val['profileimage'], 'directory' => '../' . $type, 'type' => $type!='eyes'?$type:'face');
+                        $items[] = array('id' => $val['name'], 'item_name' => $val['name'], 'profileimage_dir' => 'profilepics', 'profileimage' => $val['profileimage'], 'directory' => '../' . $type, 'type' => $type);
                     }
                 }
             }
