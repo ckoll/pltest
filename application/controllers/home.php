668852acb52a120f08aa25d1c2a359e8fe6bbce2
Alex Fedorets <santik.alex@gmail.com>
2012-11-29 13:17:27 +0200

    email resend

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,13 +3,15 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
     public $data = array();
 
     public $admins = array(2);
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         $this->tpl->gtpl = 'homepage';
         $this->load->model('home_model');
@@ -22,17 +24,17 @@ class Home extends CI_Controller {
         $this->load->model('dressup_model');
         $this->load->model('user_model');
 
-        $page = isset($_GET['page'])?(int)$_GET['page']:1;
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 
 
         $lastHeartedPhotos = $this->upload_model->get_all_last_hearted(5, $page);
         $lastCommentedPhotos = $this->upload_model->get_all_last_commented(5, $page);
-        $latestPhotos = $this->upload_model->latest_photos(4, $page-1);
-        $photosIds = (isset($_SESSION['photo_ids']) && $page != 1 ) ?(array)$_SESSION['photo_ids']:array();
+        $latestPhotos = $this->upload_model->latest_photos(4, $page - 1);
+        $photosIds = (isset($_SESSION['photo_ids']) && $page != 1) ? (array)$_SESSION['photo_ids'] : array();
 
         $topPhotos = array();
 
-        foreach($latestPhotos as $photo) {
+        foreach ($latestPhotos as $photo) {
             if (!isset($topPhotos[$photo['id']])) {
                 if (!in_array($photo['id'], $photosIds)) {
                     $topPhotos[$photo['id']] = $photo;
@@ -47,13 +49,13 @@ class Home extends CI_Controller {
         unset($topPhotos[$keys[0]]);
         //
 
-        foreach($lastHeartedPhotos as $photo) {
+        foreach ($lastHeartedPhotos as $photo) {
             if (!in_array($photo['id'], $photosIds)) {
                 $topPhotos[$photo['id']] = $photo;
                 $photosIds[] = $photo['id'];
             }
         }
-        foreach($lastCommentedPhotos as $photo) {
+        foreach ($lastCommentedPhotos as $photo) {
             if (!isset($topPhotos[$photo['id']])) {
                 if (!in_array($photo['id'], $photosIds)) {
                     $topPhotos[$photo['id']] = $photo;
@@ -63,13 +65,12 @@ class Home extends CI_Controller {
         }
 
 
-
         $_SESSION['photo_ids'] = $photosIds;
 
-        shuffle($topPhotos) ;
+        shuffle($topPhotos);
 
         //strange req from client about order
-        $place = rand(0,3);
+        $place = rand(0, 3);
         $topPhotos[$place] = $oneLastPhoto;
         //
 
@@ -77,14 +78,14 @@ class Home extends CI_Controller {
 
         $topPhotos = array_values($topPhotos);
 
-        foreach($topPhotos as $i=>$photo) {
+        foreach ($topPhotos as $i => $photo) {
             $last3Comments = $this->upload_model->get_photo_comments(3, 0, $photo['id']);
-            foreach($last3Comments as $j=>$comment) {
+            foreach ($last3Comments as $j => $comment) {
 
                 if (in_array($comment['uid'], $this->admins)) {
-                    $rand = rand(0,1);
+                    $rand = rand(0, 1);
                     $count = count($last3Comments);
-                    if($rand && $count>1) {
+                    if ($rand && $count > 1) {
                         unset($last3Comments[$j]);
                     }
                 }
@@ -99,13 +100,13 @@ class Home extends CI_Controller {
 
         $lastHeartedDressup = $this->dressup_model->get_all_last_hearted(5, $page);
         $lastCommentedDressup = $this->dressup_model->get_all_last_commented(5, $page);
-        $latestDressup = $this->dressup_model->all_latest_dressups(4, $page-1);
+        $latestDressup = $this->dressup_model->all_latest_dressups(4, $page - 1);
         $topDressup = array();
 
-        $dressupIds = (isset($_SESSION['dressup_ids']) && $page != 1)?(array)$_SESSION['dressup_ids']:array();
+        $dressupIds = (isset($_SESSION['dressup_ids']) && $page != 1) ? (array)$_SESSION['dressup_ids'] : array();
         //$dressupIds = array();
 
-        foreach($latestDressup as $photo) {
+        foreach ($latestDressup as $photo) {
             if (!isset($topDressup[$photo['id']])) {
                 if (!in_array($photo['id'], $dressupIds)) {
                     $topDressup[$photo['id']] = $photo;
@@ -120,14 +121,14 @@ class Home extends CI_Controller {
         unset($topDressup[$keys[0]]);
         //
 
-        foreach($lastHeartedDressup as $photo) {
+        foreach ($lastHeartedDressup as $photo) {
             if (!in_array($photo['id'], $dressupIds)) {
                 $topDressup[$photo['id']] = $photo;
                 $dressupIds[] = $photo['id'];
             }
 
         }
-        foreach($lastCommentedDressup as $photo) {
+        foreach ($lastCommentedDressup as $photo) {
             if (!isset($topDressup[$photo['id']])) {
                 if (!in_array($photo['id'], $dressupIds)) {
                     $topDressup[$photo['id']] = $photo;
@@ -142,20 +143,20 @@ class Home extends CI_Controller {
         shuffle($topDressup);
 
         //strange req from client about order
-        $place = rand(0,3);
+        $place = rand(0, 3);
         $topDressup[$place] = $oneLastDressup;
         //
 
         $topDressup = array_values($topDressup);
 
-        foreach($topDressup as $i=>$dressup) {
+        foreach ($topDressup as $i => $dressup) {
             if (!$this->_isNakedDressup($dressup['id'])) {
                 $last3Comments = $this->dressup_model->get_dressup_comments(3, 0, $dressup['id']);
-                foreach($last3Comments as $j=>$comment) {
+                foreach ($last3Comments as $j => $comment) {
                     if (in_array($comment['uid'], $this->admins)) {
-                        $rand = rand(0,1);
+                        $rand = rand(0, 1);
                         $count = count($last3Comments);
-                        if($rand && $count>1) {
+                        if ($rand && $count > 1) {
                             unset($last3Comments[$j]);
                         }
                     }
@@ -173,7 +174,7 @@ class Home extends CI_Controller {
 
         $maxPhotos = count($topPhotos);
         $maxDressups = count($topDressup);
-        $max = $maxPhotos>$maxDressups?$maxPhotos:$maxDressups;
+        $max = $maxPhotos > $maxDressups ? $maxPhotos : $maxDressups;
         $this->data['max'] = $max;
 
         $this->data['recently_online_users'] = $this->user_model->getRecentlyOnline(6);
@@ -202,31 +203,33 @@ class Home extends CI_Controller {
         );
         $items = $this->dressup_model->getDressupItems($id);
         $itemsTypes = array();
-        foreach($items as $item) {
+        foreach ($items as $item) {
             $itemsTypes[] = $item['type'];
         }
 
-        foreach($itemsTypes as $type) {
+        foreach ($itemsTypes as $type) {
 
         }
 
     }
 
 
-    public function login() {
+    public function login()
+    {
         $this->home_model->check_loginned_user();
         if ($this->input->get('confirmed')) {
             $this->data['err'] = 'Your email has been confirmed';
         } elseif ($this->input->get('not_confirmed')) {
             $this->data['err'] = 'Wrong activation key';
-        } elseif($this->input->get('social')){
-            $this->data['err'] = 'User '.$this->input->get('social').' not found';
+        } elseif ($this->input->get('social')) {
+            $this->data['err'] = 'User ' . $this->input->get('social') . ' not found';
         }
         $this->tpl->ltpl = array('homepage' => 'login');
         $this->tpl->show($this->data);
     }
 
-    public function signin() {
+    public function signin()
+    {
         $this->home_model->check_loginned_user();
         if ($this->input->post('signin')) {
             if ($this->home_model->check_signin_form($this->data['err'])) {
@@ -237,7 +240,8 @@ class Home extends CI_Controller {
         $this->tpl->show($this->data);
     }
 
-    public function register() {
+    public function register()
+    {
         $this->home_model->check_loginned_user();
         //invite
         if ($this->input->get('invite')) {
@@ -262,13 +266,14 @@ class Home extends CI_Controller {
         $this->tpl->show($this->data);
     }
 
-    public function forgotpassword() {
+    public function forgotpassword()
+    {
         $this->home_model->check_loginned_user();
 
         if ($this->input->post('send')) {
             $user_mail_key = $this->home_model->check_forgotpassword_form($this->data['err']);
             if ($user_mail_key) {
-                $this->home_model->send_to_mail($user_mail_key['email'], $user_mail_key['key'], 'Password reset for perfect-look.org', 'forgotpassword', 'forgotpassword',$user_mail_key['username']);
+                $this->home_model->send_to_mail($user_mail_key['email'], $user_mail_key['key'], 'Password reset for perfect-look.org', 'forgotpassword', 'forgotpassword', $user_mail_key['username']);
                 $this->data['sended'] = 1;
             }
         }
@@ -288,9 +293,11 @@ class Home extends CI_Controller {
         $this->tpl->show($this->data);
     }
 
-    public function resendemail() {
+    public function resendemail()
+    {
         $this->home_model->check_loginned_user();
         if ($this->input->post('send') && $this->input->post('email')) {
+
             //Search user
             $user = $this->db->query('SELECT * FROM users WHERE email="' . mysql_real_escape_string($this->input->post('email')) . '" OR username="' . mysql_real_escape_string($this->input->post('email')) . '"')->row_array();
             if (!empty($user)) {
@@ -300,22 +307,62 @@ class Home extends CI_Controller {
                 $this->data['sended'] = 1;
             } else {
                 //not found
-                $this->data['err'] = 'Email is not registered';
+                $this->data['err'] = 'Username does not exist';
             }
         }
         $this->tpl->ltpl = array('homepage' => 'resendemail');
         $this->tpl->show($this->data);
     }
 
+    function check_email_address($email)
+    {
+        // First, we check that there's one @ symbol,
+        // and that the lengths are right.
+        if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
+            // Email invalid because wrong number of characters
+            // in one section or wrong number of @ symbols.
+            return false;
+        }
+        // Split it into sections to make life easier
+        $email_array = explode("@", $email);
+        $local_array = explode(".", $email_array[0]);
+        for ($i = 0; $i < sizeof($local_array); $i++) {
+            if
+            (!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$",
+                $local_array[$i])
+            ) {
+                return false;
+            }
+        }
+        // Check if domain is IP. If not,
+        // it should be valid domain name
+        if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) {
+            $domain_array = explode(".", $email_array[1]);
+            if (sizeof($domain_array) < 2) {
+                return false; // Not enough parts to domain
+            }
+            for ($i = 0; $i < sizeof($domain_array); $i++) {
+                if
+                (!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$",
+                    $domain_array[$i])
+                ) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     //Login with Facebook
-    public function fb() {
+    public function fb()
+    {
         require(APPPATH . 'libraries/facebook/facebook.php');
         $this->load->config('social');
         $facebook = new Facebook(array(
-                    'appId' => $this->config->item('fbAppId'),
-                    'secret' => $this->config->item('fbSecret'),
-                    'cookie' => true
-                ));
+            'appId' => $this->config->item('fbAppId'),
+            'secret' => $this->config->item('fbSecret'),
+            'cookie' => true
+        ));
         $user = $facebook->getUser();
         if ($user) {
             try {
@@ -324,14 +371,15 @@ class Home extends CI_Controller {
                 $user = null;
                 exit;
             }
-        }else{
+        } else {
             $loginUrl = $facebook->getLoginUrl(array('req_perms' => 'email,publish_stream,read_friendlists,publish_stream'));
             redirect($loginUrl);
         }
     }
 
     //Twitter callback function for getAccessToken
-    public function tw_callback() {
+    public function tw_callback()
+    {
         require_once APPPATH . 'libraries/twitter/twitteroauth.php';
         $this->load->config('social');
 
@@ -355,7 +403,8 @@ class Home extends CI_Controller {
         $this->tw();
     }
 
-    public function tw() {
+    public function tw()
+    {
         require_once APPPATH . 'libraries/twitter/twitteroauth.php';
         $this->load->config('social');
         $tokens = $this->session->userdata('tw_session');
@@ -386,7 +435,7 @@ class Home extends CI_Controller {
             $content = $connection->get('account/verify_credentials');
             if (empty($content->error)) {
                 $this->home_model->tw_profile($content);
-            }else{
+            } else {
                 $this->session->unset_userdata('tw_session');
                 $this->tw();
             }
